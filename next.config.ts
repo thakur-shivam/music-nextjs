232f1f42e58af95c2e32b69cdcc80e6b0130a8be
ThakurShivam12 <shivamrudra2006@gmail.com>
2025-11-29 18:47:00 +0530

    add all pages and components

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['images.unspalsh.com', 'res.cloudinay.com']
+  }
 };
 
 export default nextConfig;
